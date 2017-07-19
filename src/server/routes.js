915e768d6f9c313d6fb2d7ea70653a6513d94291
John Papa <john@Johns-MBP.lan>
2017-07-19 08:32:04 -0400

    comment

--- src/server/routes.js
+++ src/server/routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const heroService = require('./hero.service');
 
 router.get('/heroes', (req, res) => {
-  // res.send(200, [{ id: 10, name: 'Starlord', saying: 'oh yeah' }]);
   heroService.getHeroes(req, res);
 });
 
